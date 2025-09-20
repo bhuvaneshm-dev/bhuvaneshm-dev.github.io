d7eaf593e0510c546e1932d4f9f7754f735d20c8
bhuvaneshm-dev <bhuvaneshmuniraj2020@gmail.com>
2025-09-20 11:45:07 +0530

    Delete replybox directory

--- replybox/skytalker/version/og-skytalker/data/e.js
+++
@@ -1,9 +0,0 @@
-window.data = {
-    "earth": {
-        text: "Earth is the third planet from the Sun and the only known celestial body to support life...",
-        url: "https://www.ecosia.org/search?q=earth",
-        blog: "https://bhuvaneshm-dev.blogspot.com/2025/01/10facts-about-the-earth.html",
-        imageUrl: "https://3.bp.blogspot.com/-EAV8woA9wCk/TVV4Vd2AWII/AAAAAAAAAz8/DDnLno6zQeU/s1600/planet-earth.jpg"
-    }
-};
-
